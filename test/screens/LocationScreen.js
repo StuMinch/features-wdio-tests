f2f463c6bb0c358cb8dd5b05fc810e878be1c08c
Stuart Minchington <stuart.minchington@saucelabs.com>
2026-01-05 16:15:08 -0800

    Created and tested the new geolocation tests

---
+++ test/screens/LocationScreen.js
@@ -0,0 +1,8 @@
+// test/screens/LocationScreen.js
+class LocationScreen {
+    get locationPermissionButton() {
+      return $('~Allow While Using App');
+    }
+  }
+  
+  export default new LocationScreen(); 
